4ea9e1076ee7858846f7d51b8feeee588a5ee0f7
spankingcode <trudnehaslo123A>
2017-02-06 22:29:40 +0100

    mobile first - already 95% missing fixed nav  and portfolio pseudoclass( hoover etc)

--- assets/main-script.js
+++ assets/main-script.js
@@ -2,7 +2,13 @@
  * Created by HP-M on 02.02.2017.
  */
 
+$(window).on("load", function(){
+    $(".preloader").fadeOut(500);
+});
+
+
 $(document).ready(function () {
+
     $(".social").click(function () {
         $("social").toggle("slow");
     });
