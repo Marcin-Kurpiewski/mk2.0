2dcf6021fe5f0ea0b613ce2539a384a55eddb752
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-02-25 13:40:24 +0100

    media queries min width 768 miss font sizes

--- assets/main-script.js
+++ assets/main-script.js
@@ -7,34 +7,14 @@
 // });
 //
 //
-// $(document).ready(function () {
-//
-//     $(".social").click(function () {
-//         $("social").toggle("slow");
-//     });
-// });
+$(document).ready(function () {
+    $(".burger__btn").on("click", function () {
+        $("nav").toggleClass('nav');
+    });
 
-//
-// $(document).ready(function () {
-//     $(".burger__btn").on("click", function () {
-//             $(".burger__btn").toggle(function () {
-//                     $(".nav").show();
-//                 },
-//                 function () {
-//                     $(".nav").hide();
-//                 });
-//         });
-//
-// });
+});
 
-// var svg = new Walkway(options);
-// // Draw when ready, providing an optional callback
-// svg.draw(callback);
-//
-// // Options passed in as an object, see options below.
-// var svg = new Walkway({ selector: '#test'});
-//
-// // Overwriting defaults
+
 var svg = new Walkway({
     selector: '#test',
     duration: '3000',
