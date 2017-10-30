85d170cc028305e953784a2e3ea1c858f93c4443
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-10-30 20:33:29 +0100

    'refactor'

--- assets/main-script.js
+++ assets/main-script.js
@@ -1,7 +1,4 @@
-
 $(document).ready(function () {
-
-
     $('.nav-item').click(
         function (event) {
             event.preventDefault();
@@ -10,35 +7,31 @@ $(document).ready(function () {
             }, 800)
         });
 
+    function burgerMenu() {
 
-    function burgerMenu(){
-
-        var menuElem= document.getElementById('brg_nav');
+        var menuElem = document.getElementById('brg_nav');
 
         event.preventDefault();
-        if(menuElem.className==='close-nav'){
-            menuElem.className='open-nav';
+        if (menuElem.className === 'close-nav') {
+            menuElem.className = 'open-nav';
         } else {
-            menuElem.className='close-nav';
+            menuElem.className = 'close-nav';
         }
     }
 
-    var brgButton= document.getElementById('brg_btn');
-
+    var brgButton = document.getElementById('brg_btn');
     brgButton.addEventListener('click', burgerMenu, false);
-
-
 
     function temporaryInformation() {
         var windowWidth = window.innerWidth;
-        if (windowWidth >= 1440) {
+        if (windowWidth >= 2550) {
             var newDiv = document.createElement('div');
             newDiv.className = "temp";
             var newP = document.createElement('p');
             newP.className = "info";
             var addTxt = document.createTextNode("Ooopsss max resolution below 1440 px, ");
-            var addTxt1= document.createTextNode("the rest of the build.... ");
-            var addTxt2= document.createTextNode("sorry about it :)");
+            var addTxt1 = document.createTextNode("the rest of the build.... ");
+            var addTxt2 = document.createTextNode("sorry about it :)");
             newP.appendChild(addTxt);
             newP.appendChild(addTxt1);
             newP.appendChild(addTxt2);
@@ -49,8 +42,8 @@ $(document).ready(function () {
             newDiv.appendChild(newP);
             newDiv.appendChild(newImg);
             var position = document.getElementsByTagName('body')[0];
-            var positionBefore= document.getElementsByTagName('footer')[0];
-            position.insertBefore(newDiv,positionBefore);
+            var positionBefore = document.getElementsByTagName('footer')[0];
+            position.insertBefore(newDiv, positionBefore);
 
         }
     };
