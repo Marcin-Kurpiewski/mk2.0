cef8ae68b47b8842a49e3767087ca971d47536d1
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-10-30 22:37:18 +0100

    'rem-back'

--- assets/main-script.js
+++ assets/main-script.js
@@ -19,35 +19,35 @@ $(document).ready(function () {
         }
     }
 
-    var brgButton = document.getElementById('brg_btn');
-    brgButton.addEventListener('click', burgerMenu, false);
-
-    function temporaryInformation() {
-        var windowWidth = window.innerWidth;
-        if (windowWidth >= 2550) {
-            var newDiv = document.createElement('div');
-            newDiv.className = "temp";
-            var newP = document.createElement('p');
-            newP.className = "info";
-            var addTxt = document.createTextNode("Ooopsss max resolution below 1440 px, ");
-            var addTxt1 = document.createTextNode("the rest of the build.... ");
-            var addTxt2 = document.createTextNode("sorry about it :)");
-            newP.appendChild(addTxt);
-            newP.appendChild(addTxt1);
-            newP.appendChild(addTxt2);
-            var newImg = document.createElement('img');
-            newImg.className = "img_info";
-            newImg.src = "assets/images/mobile_info.png";
-
-            newDiv.appendChild(newP);
-            newDiv.appendChild(newImg);
-            var position = document.getElementsByTagName('body')[0];
-            var positionBefore = document.getElementsByTagName('footer')[0];
-            position.insertBefore(newDiv, positionBefore);
-
-        }
-    };
-
-    temporaryInformation();
+    // var brgButton = document.getElementById('brg_btn');
+    // brgButton.addEventListener('click', burgerMenu, false);
+    //
+    // function temporaryInformation() {
+    //     var windowWidth = window.innerWidth;
+    //     if (windowWidth >= 2550) {
+    //         var newDiv = document.createElement('div');
+    //         newDiv.className = "temp";
+    //         var newP = document.createElement('p');
+    //         newP.className = "info";
+    //         var addTxt = document.createTextNode("Ooopsss max resolution below 1440 px, ");
+    //         var addTxt1 = document.createTextNode("the rest of the build.... ");
+    //         var addTxt2 = document.createTextNode("sorry about it :)");
+    //         newP.appendChild(addTxt);
+    //         newP.appendChild(addTxt1);
+    //         newP.appendChild(addTxt2);
+    //         var newImg = document.createElement('img');
+    //         newImg.className = "img_info";
+    //         newImg.src = "assets/images/mobile_info.png";
+    //
+    //         newDiv.appendChild(newP);
+    //         newDiv.appendChild(newImg);
+    //         var position = document.getElementsByTagName('body')[0];
+    //         var positionBefore = document.getElementsByTagName('footer')[0];
+    //         position.insertBefore(newDiv, positionBefore);
+    //
+    //     }
+    // };
+    //
+    // temporaryInformation();
 
 });
